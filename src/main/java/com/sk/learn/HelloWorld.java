6f8a9b6b3c079def88cd9fdf81c94790491b0708
Shankar Modi <shankar.modi08@gmail.com>
2016-10-30 23:13:10 +0530

    Committing first program.

---
+++ src/main/java/com/sk/learn/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.sk.learn;
+
+/**
+ * 
+ * @author Shankar Modi
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("This is the first java program.");
+	}
+}
